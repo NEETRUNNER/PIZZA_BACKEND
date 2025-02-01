cffbc29001117ea72827549eb14b895bc49de816
Алексей <Bbiba6907@gmail.com>
2025-02-01 16:36:44 +0200

    fixed again

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const express = require('express');
 const app = express()
 
 const cors = require('cors')
-app.use(cors())
+app.use(cors({
+    origin: 'https://neetrunner.github.io/PIZZA/#/'
+}))
 
 const AuthRouter = require('./authRouter')
 
