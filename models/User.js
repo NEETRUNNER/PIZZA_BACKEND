9c5a0c29e0dc0b04812a9d3cd2b06ed5d8f2b0e0
Алексей <Bbiba6907@gmail.com>
2025-02-01 13:28:03 +0200

    update user

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const User = new Schema ({ // Создаём схему-сущность какими должны быть данные пользователя
-    username: {type: String, unique: true, required: true},
+    email: {type: String, unique: true, required: true},
     password: {type: String, required: true},
     roles: [{type: String, ref: 'Role'}]
 })
