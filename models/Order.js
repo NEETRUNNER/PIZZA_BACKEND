b65ec4aecfb542cfd1ab966ea86f1e1fc2e97d42
Алексей <Bbiba6907@gmail.com>
2025-02-10 14:07:41 +0200

    Order add

--- models/Order.js
+++ models/Order.js
@@ -4,6 +4,7 @@ const OrderSchema = new Schema ({
     name: {type: String, require: true},
     address: {type: String, require: true},
     phone: {type: String, require: true},
+    pizzas: {type: Object, require: true},
     orderTime: {type: String, require: true}
 })
 
